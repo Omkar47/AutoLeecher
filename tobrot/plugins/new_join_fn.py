8b730d9ceb55a82f9f7fb3b8d9f39f6094739904
Omkar Londhe <49039825+Omkar47@users.noreply.github.com>
2021-05-01 20:43:42 +0530

    Update new_join_fn.py

--- tobrot/plugins/new_join_fn.py
+++ tobrot/plugins/new_join_fn.py
@@ -15,14 +15,25 @@ import pyrogram
 
 
 from tobrot import (
-    AUTH_CHANNEL
+    AUTH_CHANNEL,
+    CANCEL_CMD,
+    LEECH_CMD,
+    YTDL_CMD,
+    STATUS_CMD,
+    STATS_CMD,
+    LOG_CMD,
+    SAVE_CMD,
+    DELETE_CMD,
+    HELP_CMD,
+    UPLOAD_CMD,
+    EXEC_CMD
 )
 
 
 async def new_join_f(client, message):
     chat_type = message.chat.type
     if chat_type != "private":
-        await message.reply_text(f"Current CHAT ID: <code>{message.chat.id}</code>")
+        await message.reply_text(f"Current CHAT ID: <code>{message.chat.id}</code>\n\nYou Can Try Checking @APDLeechBoX")
         # leave chat
         await client.leave_chat(
             chat_id=message.chat.id,
@@ -32,15 +43,14 @@ async def new_join_f(client, message):
     await message.delete(revoke=True)
 
 
-async def help_message_f(client, message):
-    # await message.reply_text("no one gonna help you 🤣🤣🤣🤣", quote=True)
-    channel_id = str(AUTH_CHANNEL)[4:]
-    message_id = 99
-    # display the /help message
-    await message.reply_text(
-        f"please read the <a href='https://t.me/c/{channel_id}/{message_id}'>Pinned Message</a>",
-        quote=True
-    )
+async def help_msg(client, message):
+    await message.reply_text("<b>Read Pinned Message For More help & to Avoid Ban</b>\n\n/help : To See This message" \
+    f"\n\n<code>/{LEECH_CMD}</code> : To Leech URI" \
+    f"\n\n<code>/{YTDL_CMD}</code> : To Download ytdl Supported Sites" \
+    f"\n\n<code>/{SAVE_CMD}</code> : To Save Thumbnail" \
+    f"\n\n<code>/{DELETE_CMD}</code> : To Clear Thumbnail" \
+    f"\n\n<code>/{LEECH_CMD} archive</code> : To Tar Files" \
+    f"\n\n<code>/{LEECH_CMD} unzip</code> : To Unzip/Untar/Unrar Files", quote=True)
 
 
 async def rename_message_f(client, message):
