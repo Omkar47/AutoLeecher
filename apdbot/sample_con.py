d0aeb8d3186547019584bc8bc98fd5ca1a2c5ecf
Omkar Londhe <49039825+Omkar47@users.noreply.github.com>
2021-08-12 22:33:45 +0530

    Fixing :^)

--- apdbot/sample_con.py
+++ apdbot/sample_con.py
@@ -2,7 +2,7 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    TG_BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
     # The Telegram API things
     APP_ID = int(os.environ.get("APP_ID", 12345))
     API_HASH = os.environ.get("API_HASH")
